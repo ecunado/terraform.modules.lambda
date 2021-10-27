f795d50cc32f717a8913c07077b4274fd1e175f4
ecunado <ecunado@handpoint.com>
2021-10-27 19:39:03 +0200

    WIP

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-output "lambda_arn" { value = element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_vpc.*.arn,tolist([""])), 0) }
+output "lambda_arn" { 
+  value = element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_vpc.*.arn,tolist([""])), 0) 
+}
+
+output "invoke_arn" {
+  value = element(concat(aws_lambda_function.lambda.*.invoke_arn, aws_lambda_function.lambda_with_vpc.*.invoke_arn,tolist([""])), 0) 
+}
